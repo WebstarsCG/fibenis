f7067c7abaf500af31d9e825381467caed13ff79
Raja Renga Bashyam <raja@webstarscg.com>
2020-06-22 10:59:17 +0530

    Minor cleanup to share for kalycito

--- nano/fE7zRhHqYfSLT9CRm55cBPGHjAGuhqhhjKGSZrB.php
+++ nano/fE7zRhHqYfSLT9CRm55cBPGHjAGuhqhhjKGSZrB.php
@@ -28,7 +28,7 @@
                         
                         "engine"        => 'eav',
                         
-                        "is_open"       => 1,
+                        "is_open"       => 0,
                         #'avoid_gate'    => 1,
                         
                         "access_key"    => "fortknox",
