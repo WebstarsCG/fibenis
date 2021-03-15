b4b5d1fa975190df5a7e170e7fd5e8c1021eea1c
Raja Renga Bashyam <raja@webstarscg.com>
2021-03-15 19:15:58 +0530

    Update on c3 wrapper file to accomodate x->axis custom labeling based data set

--- lib/v_01/def/entity_dash_board/d.php
+++ lib/v_01/def/entity_dash_board/d.php
@@ -2,55 +2,71 @@
 
     $D_SERIES   =   array(
 	
-			    'title' => 'Entity Dashboard',
+							'title' 	=> 'Entity Dashboard',
 	
-			    'data'=> array(
-				
-					    1=> array(
-						       'field_composite'=>  [  
-										'fields'=>['x'    => ' (SELECT sn FROM entity WHERE code=entity_code)',
-											   'Total'=> 'count' ],
-										'table' =>'entity_count'
-									    ],
-						       
-						       'js_call'	=> 'c3001.graphDonut',
-						       'attr' 		=> [	'class'	   	=> "col-md-12 pad_lr mar_top_25",
-										'data-title' 	=> "Entity Count",
-										'data-type'  	=> 'bar',
-									    ]
-					    ),
+							'data'		=> array(
+							
+												1=> array(
+													   
+													   'field_composite'=> [  
+																
+																'fields'=>[ 'x'	   => ' (SELECT sn FROM entity WHERE code=entity_code)',
+																			'Total'=> 'count' ],
+														
+																'table' =>'entity_count' 
+														],
+													   
+													   'js_call'	=> 'c3001.graphDonut',
+													   
+													   'attr' 		=> [	'class'	   	=> 'col-md-12 pad_lr mar_top_25',
+																			'data-title'=> 'Entity Count',
+																		
+																		]
+												),
+						
+												2=> array(
+													  
+													   'field_composite'=>  ['fields'=>['Users'		=> 'page_action',
+																						'Sessions'	=> 'total_count'],
+																			 'table' => 'page_access_count'],													   
+													   'js_call'		=> 'c3001.graphBase',													   
+													   'attr' 			=> ['class'      	=> "col-md-12 pad_lr mar_top_25",
+																			'data-title' 	=> "Page Access Count",
+																			'data-type'  	=> 'area',
+																			#'data-bar-width'=> 50,
+																			'data-grid-x'	=> true,
+																			'data-grid-y'	=> true]
+													   ),	
 						
 						
-					    2=> array(
+					    3=> array(
 						      
 						       'field_composite'=>  [
 									      
-									      'fields'=>['Date'=>'date',
-											 'Total'=>'total',
-											// 'Total 20'=>'(total+20)',
-											// 'Rand'=>"(ROUND((RAND()*(30-5)+5),0))"
-											 ],
+									      'fields'=>['x'=>'date',
+													'Total'=>'total'
+											],
 									      
 									      'table' => 'page_view_log_by_day',
-									      'filter'=> " page_code='f6a493b8ef6b14b86c060917f15595c4' ORDER BY DATE ASC "
+									      'filter'=> " page_code='7f84cf049cd1ca1ec04c0150d4d0c356' ORDER BY DATE ASC "
 									    ],
 						       
-						       'js_call'	=> 'c3001.graphBase',
+						       'js_call'	=> 'c3001.timeSeries',
 						       
 						       'attr' 		=> [
 								  
 										'class'      	=> "col-md-12 pad_lr mar_top_25",
 										'data-title' 	=> "Login Count by Day",
-										'data-type'  	=> 'line',
+										'data-type'  	=> 'area',
 										#'data-bar-width'=> 50,
 										'data-grid-x'	=> true,
 										'data-grid-y'	=> true
 									    ]
-						       ),							
+						       ),						
 						
 					), # columns
 			    			    
 			    'mode' 	=> 'graph',
-			    
+			  
 		    );    
 ?>
