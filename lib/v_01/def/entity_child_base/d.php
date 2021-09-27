b2a9a3d5f71181c6c3dd082384d91976ea23aeae
tharani <tharani@webstarscg.com>
2021-09-27 17:09:38 +0530

    [IMP]Improvements in entity child base

--- lib/v_01/def/entity_child_base/d.php
+++ lib/v_01/def/entity_child_base/d.php
@@ -9,139 +9,105 @@
 					'data'=> array(
 					                
 							1=>array('th'      => 'Entity',
-								 'field'   => '(SELECT sn FROM entity WHERE entity.code=entity_code)',
-								 'td_attr' => ' class="clr_gray_8 align_LM" width="15%"',
-								 'is_sort' => 1),
-													
-                                                        2=>array('th'		=> 'Token',									 
-								 'field'	=> 'token',								
-								 'is_sort' 	=> 1,								
-								 'td_attr' 	=> ' class="label_father align_LM" width="10%"'								),
+									 'field'   => '(SELECT sn FROM entity WHERE entity.code=entity_code)',
+									 'td_attr' => ' class="clr_gray_8 align_LM" width="15%"',
+									 'is_sort' => 1),
+														
+                            2=>array('th'		=> 'Token',									 
+									 'field'	=> 'token',								
+									 'is_sort' 	=> 1,								
+									 'td_attr' 	=> ' class="label_father align_LM" width="10%"'								
+									 ),
+								
 							
-							3=>array('th'		=> 'Short Name',								 
-								'field'		=> 'sn',								     															
-								'is_sort'	=> 1,								
-								'td_attr' 	=> ' width="25%" '), 
-						
-							// 
-							//4=>array('th'=>'Long Name',
-							//		 
-							//	'field'=>'ln',
-							//	
-							//	'is_sort' => 1,
-							//		 
-							//	'td_attr' => ' class="label_child align_LM" width="25%"',
-							//		 
-							//	),
+							3=>array('th'		=>'Name',
+									'field'		=>'ln',
+									'is_sort' 	=> 1,
+									'td_attr' 	=> ' class="label_child align_LM" width="25%"',
+										 
+								),
+								
+							4=>array('th'		=> 'Exav addon suffix',								 
+									'field'		=> 'sn',								     															
+									'is_sort'	=> 1,								
+									'td_attr' 	=> ' width="25%" '
+									), 
+							
 							
 							5=>array('th'=>'DNA',
-									 
-								#'field'=>'(SELECT ecb.sn FROM entity_child_base as ecb WHERE ecb.token=dna_code)',
-								'field'=>'dna_code',
-								
-								'is_sort' => 1,
-									 
-								'td_attr' => ' class="label_father" width="10%"',
-									 
+									#'field'	=>'(SELECT ecb.sn FROM entity_child_base as ecb WHERE ecb.token=dna_code)',
+									'field'		=>'dna_code',
+									'is_sort' 	=> 1,
+									'td_attr' 	=> ' class="label_father" width="10%"',
 								),
 							
-							6=>array('th'=>'Line order',
-								 
-								'field'=>'line_order',
-								     															
-								'is_sort'      => 1,
-									
-								'td_attr' => ' width="5%" ',
-							                                                                     
+							6=>array('th'		=>'LO',
+									'field'		=>'line_order',
+									'is_sort'   => 1,
+									'td_attr' 	=> ' width="5%" ',
 								),
 							
-							8=>array('th'=>'Updation',
-									 
-								'field'=>"concat(get_user_internal_name(user_id),',',date_format(timestamp_punch,'%d-%b-%y %T'))",
-							        									 
-								'td_attr' => 'width="20%"',
-								
-								'js_call'=> 'show_user_info',
-								
-								'is_sort' => 'timestamp_punch'
-									 
+							8=>array('th'		=>'Updation',
+									'field'		=>"concat(get_user_internal_name(user_id),',',date_format(timestamp_punch,'%d-%b-%y %T'))",
+									'td_attr' 	=> 'width="20%"',
+									'js_call'	=> 'show_user_info_2l',
+									'is_sort' 	=> 'timestamp_punch'
+										 
 								),
-								
                                                     ),
 				    
-                                    
-                                       'action' => array('is_edit' =>1),
-                                       
-                                       'order_by'   =>' ORDER BY token ASC ' ,
-				       		
+                                    'action' 	=> array('is_edit' =>1),
+                                    'order_by' =>' ORDER BY token ASC ' ,
                                 
                                     #Table Info
-                                    
-                                    'table_name' =>'entity_child_base',
-                                    
+                                    'table_name'=>'entity_child_base',
                                     'key_id'    =>'id',
 				    
                                     
                                     # Default Additional Column
-                                
-                                    'is_user_id'       => 'user_id',
+                                    'is_user_id'=> 'user_id',
                                 
                                     # Communication
-                                
-                                    'prime_index'   => 3,
+                                    'prime_index'=> 3,
 				    
 				    'search'=> 	array(
 							  
 							
-							array(  'data'  =>array('table_name' 	=> 'entity_child_base',
-										'field_id'	=> 'token',
-										'field_name' 	=> 'token',
-										'filter'	=> "AND dna_code IN ('EBUC','EBMS','EBAX','EBFA')"
-										
-									 ),
-							      
-								'title' 		=> 'Token',										
-								'search_key' 		=> 'token',													       
-								'is_search_by_text' 	=> 1, //( For Text search case)	      
-							),
-							
-								
+								array(  'data'  		  =>array('table_name' 	=> 'entity_child_base',
+																'field_id'		=> 'token',
+																'field_name'	=> 'token',
+																'filter'		=> "AND dna_code IN ('EBUC','EBMS','EBAX','EBFA')"
+																
+																 ),
+									  
+										'title' 		   => 'Token',										
+										'search_key' 	   => 'token',													       
+										'is_search_by_text'=> 1, //( For Text search case)	      
+									),
 						),
 				    
-				    'custom_filter' => array(  			     						   
-							      
-									array(  'field_name' => 'Entity:',
-									      
-										'field_id' => 'cf1', // 
-										
-										'filter_type' =>'option_list', 
-												    
-										'option_value'=> $G->option_builder('entity','code,sn'," ORDER BY sn ASC"),
-									  
-										'html'=>'  title="Select Client"   data-width="160px"  ',
-									   
-										'cus_default_label'=>'Show All',
-									  
-										'filter_by'  => "entity_code"  // main table value
-									),
-									
-									
-									array(  'field_name' => 'DNA:',
-									      
-										'field_id' => 'cf2', // 
-										
-										'filter_type' =>'option_list', 
-												    
-										'option_value'=> $G->option_builder('entity_child_base','token,sn',"WHERE entity_code='EB' AND dna_code='EBMS' ORDER BY sn ASC"),
-									  
-										'html'=>'  title="Select Client"   data-width="160px"  ',
-									   
-										'cus_default_label'=>'Show All',
-									  
-										'filter_by'  => "dna_code"  // main table value
-									),
-							),
-				
+				'custom_filter' => array(  			     						   
+							  
+								array('field_name' 		=> 'Entity:',
+									'field_id' 			=> 'cf1', // 
+									'filter_type' 		=>'option_list', 
+									'option_value'		=> $G->option_builder('entity','code,sn'," ORDER BY sn ASC"),
+									'html'				=>'  title="Select Client"   data-width="160px"  ',
+									'cus_default_label'	=>'Show All',
+									'filter_by'  		=> "entity_code"  // main table value
+								),
+								
+								
+								array(  'field_name' 	=> 'DNA:',
+									'field_id' 			=> 'cf2', // 
+									'filter_type' 		=>'option_list', 
+									'option_value'		=> $G->option_builder('entity_child_base','token,sn',"WHERE entity_code='EB' AND dna_code='EBMS' ORDER BY sn ASC"),
+									'html'				=>'  title="Select Client"   data-width="160px"  ',
+									'cus_default_label'	=>'Show All',
+									'filter_by'  		=> "dna_code"  // main table value
+								),
+						),
+			
                                 
                                     # File Include
                                 
@@ -151,26 +117,16 @@
 				
 				#summary:
 				
-				'summary_data'=>array(
-							array(  'name'=>'No.','field'=>'count(id)','html'=>'class=summary'),
-				
-				                   ),
-				
+					'summary_data'	=>array(
+									array('name'=>'No.','field'=>'count(id)','html'=>'class=summary'),
+										   ),
 				#check_field
-								
 					#'check_field'   =>  array('user_id' => @$_GET['user_id'],'page_code' => @$_GET['page_code']),								
-								
-					'add_button' => array( 'is_add' =>1,'page_link'=>'f=entity_child_base', 'b_name' => 'Add Child Base' ),
-								
-					'del_permission' => array('able_del'=>1), 
-								
-					
-				#export data
+					'add_button' 	=> array( 'is_add' =>1,'page_link'=>'f=entity_child_base', 'b_name' => 'Add Child Base' ),
+					'del_permission'=> array('able_del'=>1), 
 				
-					'show_query'   =>0,
-					
-					
-				                            
+				#export data
+					'show_query'  	=>0,
                             );
 	
 	if(@$_GET['default_addon']){
