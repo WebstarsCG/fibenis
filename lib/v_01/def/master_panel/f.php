d928b40d79e77aa9656e38ef5d853893c342897a
nano.webt20.com <raja@webstarscg.com>
2023-10-19 08:03:01 -0400

    [UPD] addition of CH entity and key ekv_session

--- lib/v_01/def/master_panel/f.php
+++ lib/v_01/def/master_panel/f.php
@@ -2,10 +2,20 @@
 	include($LIB_PATH."def/entity_key_value/f.php");
 
 	$F_SERIES['default_fields'] = array("entity_code"=>"'MP'");
-	
-	unset($F_SERIES['data'][1]);
-	
 	$F_SERIES['back_to']['back_link'] = '?d=master_panel';
+	$F_SERIES['is_cc'] = 1;
 
+	// unset fields	
+	unset($F_SERIES['data'][1]);
+
+	// after update 
+	$F_SERIES['after_add_update'] = function($key_id){
+			
+		global $G,$COACH;
+		// update ekv_session
+		$G->setCEKV(@$_POST['X2'],'CH','ekv_session',$G->hashKeyGenerator('AA','ZZ'));
+
+	};
+	
 
 ?>
